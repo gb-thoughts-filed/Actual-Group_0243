fea10061506254c0f1decd0123459bac71f8a38a
jeanineohene-agyei <jeaninecoa@gmail.com>
2022-06-15 22:15:20 -0400

    phase0

--- phase0/src/UserAccount.java
+++ phase0/src/UserAccount.java
@@ -1,2 +1,40 @@
-public class UserAccount {
+import java.util.ArrayList;
+import java.util.List;
+public class UserAccount{
+    private String username;
+    private String password;
+
+    private boolean admin;
+
+    public UserAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+        admin = false;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setPassword(String name) {
+        this.password = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String changePassword(String newPassword) {
+        String oldPassword = this.password;
+        this.password = newPassword;
+        return oldPassword;
+    }
+
+    public List<Object> loginHistory(){
+        return new ArrayList<>();
+    }
 }
