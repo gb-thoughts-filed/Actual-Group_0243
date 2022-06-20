9a881d0b7846a99d9be37f01812334d89769e178
arshhrelan <arshh.relan@mail.utoronto.ca>
2022-06-19 22:26:47 -0400

    push

--- phase0/test/AccountSystemTest.java
+++ phase0/test/AccountSystemTest.java
@@ -1,2 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class AccountSystemTest {
+
+    @Test(timeout = 50)
+    public void testCreateUser() {
+        CreateUserAccount newUser = new CreateUserAccount("John", "123456789", false);
+        UserAccount user = newUser.createUserAccount();
+        if (user != null) {
+            assertTrue(AccountSystem.allUsers.contains(user));
+        }
+    }
+
 }
