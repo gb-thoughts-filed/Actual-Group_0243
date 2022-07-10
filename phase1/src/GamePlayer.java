a1b7cf736b667b4ea2dffd219c993e671e393551
jeanineohene-agyei <jeaninecoa@gmail.com>
2022-07-10 12:22:02 -0400

    phase 1

---
+++ phase1/src/GamePlayer.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GamePlayer{
+    private int totalScore;
+    private String name;
+    private final Point location;
+
+    public GamePlayer(String name){
+        this.totalScore = 0;
+        // this.name = username from account
+        location = new Point(0, 0);
+
+    }
+
+    public void setTotalScore(int score){
+        this.totalScore = score;
+    }
+
+    public int getTotoalScore(){
+        return this.totalScore;
+    }
+
+    public void setLocation(double xPos, double yPos){
+        this.location.setLocation(xPos, yPos);
+    }
+
+    public List<Double> getLocation(){
+        List<Double> coordinates = new ArrayList<Double>();
+        double xPos = this.location.getX();
+        double yPos = this.location.getY();
+        coordinates.add(xPos);
+        coordinates.add(yPos);
+
+        return coordinates;
+    }
+
+    public void moveUp(){
+        double newLocation = location.getY() + 2.0;
+        location.move(location.x, (int) newLocation);
+    }
+
+    public void moveDown(){
+        double newLocation = location.getY() - 2.0;
+        location.move(location.x, (int) newLocation);
+    }
+
+
+}
