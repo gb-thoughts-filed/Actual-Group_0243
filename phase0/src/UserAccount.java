1a8dd9899605e265d9a38ce57b64bf1240584f29
huangn12 <nathaniel.huang@mail.utoronto.ca>
2022-07-12 13:26:49 -0700

    phase1

--- phase0/src/UserAccount.java
+++ phase0/src/UserAccount.java
@@ -18,7 +18,7 @@ public class UserAccount{
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getUsername() {
         return this.username;
     }
