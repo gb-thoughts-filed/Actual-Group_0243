ef0f3917e80b07e48b86fb312e94fe00c56737e0
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2022-07-12 17:01:58 -0400

    Gabrielle - ability to create a reward, I believe I should create specifics instances of reward that has methods that should also implement reward. Will discuss with groupmates.

--- phase1/src/Rewards.java
+++ phase1/src/Rewards.java
@@ -1,4 +1,14 @@
 
-public class Rewards {
+public class Reward {
 
+    private static int x_coord;
+
+    private static int y_coord;
+
+
+    public Reward(int start_x, int start_y){
+        this.x_coord = start_x;
+        this.y_coord = start_y;
     }
+
+}
