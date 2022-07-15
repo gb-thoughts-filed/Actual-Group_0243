2a84aa4e191c4ab9bef8c7dee43ee9ad0f67ae86
Gabrielle Browne <gabrielle.browne@mail.utoronto.ca>
2022-07-15 16:42:43 -0400

    -just Point for rewards created

--- phase1/src/Rewards.java
+++ phase1/src/Rewards.java
@@ -1,9 +1,13 @@
-
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Rewards {
 
     private static int x_coord;
 
     private static int y_coord;
+
+    private Point reward_location = new Point(x_coord, y_coord);
 
 
     public Rewards(int start_x, int start_y){
